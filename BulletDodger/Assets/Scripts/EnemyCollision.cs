230e6b1e1f0437aeb09729ceb7c1f41a3d9c6648
KangSoon <tankangsoon@gmail.com>
2015-10-12 17:55:38 -0700

    Woohoo complete game scene

---
+++ BulletDodger/Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyCollision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other){
+
+		if (other.collider.name == "Player") {
+			GameScript.health--;
+			Vector3 position = gameObject.transform.position;
+			SpecialEffectsScript.MakeExplosion ((position));
+			Destroy (gameObject);
+		}
+	}
+
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	}
+}
---
+++ BulletDodger/Assets/Scripts/GameScript.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GameScript : MonoBehaviour
+{
+
+	public GameObject player;
+	public GameObject enemy;
+	public Text score;
+	public GameObject panel;
+
+	private Dictionary<int, GameObject> trails = new Dictionary<int, GameObject>();
+	private float timer;
+	public static int health;
+
+	void Awake() {
+		panel.SetActive (false);
+		timer = Time.time + 2;
+		health = 3;
+		score.GetComponent<Text> ();
+	}
+	
+	void Update() {
+
+		if (health == 0) {
+			Vector3 position = player.transform.position;
+			SpecialEffectsScript.MakeExplosion ((position));
+			SpecialEffectsScript.MakeExplosion ((position));
+			SpecialEffectsScript.MakeExplosion ((position));
+			panel.SetActive(true);
+			Destroy(player);
+		}
+
+		if (timer < Time.time && health!=0) {
+			Vector2 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Screen.height));
+			Instantiate (enemy, screenPosition, Quaternion.identity);
+			timer = Time.time + 0.1f;
+			score.text = Mathf.Round (Time.time-1).ToString ();
+		}
+
+		for (int i = 0; i < Input.touchCount; i++) {
+			Touch touch = Input.GetTouch(i);
+			if (touch.phase == TouchPhase.Ended && touch.tapCount == 1){
+				Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+			}
+			if (touch.phase == TouchPhase.Began)
+			{
+				// Store this new value
+				if (trails.ContainsKey(i) == false)
+				{
+					Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+					position.z = 0; // Make sure the trail is visible
+					
+					GameObject trail = SpecialEffectsScript.MakeTrail(position);
+					
+					if (trail != null)
+					{
+						Debug.Log(trail);
+						trails.Add(i, trail);
+					}
+				}
+			}
+			else if (touch.phase == TouchPhase.Moved)
+			{
+				// Move the trail
+				if (trails.ContainsKey(i))
+				{
+					GameObject trail = trails[i];
+					
+					Camera.main.ScreenToWorldPoint(touch.position);
+					Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+					position.z = 0; // Make sure the trail is visible
+					
+					trail.transform.position = position;
+				
+					player.transform.position = position;
+				}
+			}
+			else if (touch.phase == TouchPhase.Ended)
+			{
+				// Clear known trails
+				if (trails.ContainsKey(i))
+				{
+					GameObject trail = trails[i];
+					
+					// Let the trail fade out
+					Destroy(trail, trail.GetComponent<TrailRenderer>().time);
+					trails.Remove(i);
+				}
+			}
+		}
+	}
+}
